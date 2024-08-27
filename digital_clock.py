8dc3ca865de54c84b5a971d7cdeefd708e3c3b1e
CreativeAnamika <narayantiwari1976@gmail.com>
2024-08-27 21:10:39 +0530

    Add files via upload

---
+++ digital_clock.py
@@ -0,0 +1,18 @@
+import tkinter as tk
+from time import strftime
+ 
+ 
+root=tk.TK()
+root.title("Digital Clock")
+
+def time():
+    string=strftime('%H:%M%S %P \n %D') 
+    label.config(text=string)
+    label.after(1000,time)
+    
+label=tk.label(root,font=('calibri',50,'bold'),background='yellow',foreground='black')    
+label.pack(anchor='center')
+
+time()
+
+root.mainloop()
